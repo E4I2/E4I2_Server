57bc4cafe9b5ab9ce901cc82fcb2ca30051871b4
PeachCoolpis <rlglxh@naver.com>
2024-07-20 16:31:46 +0900

    개발 환경설정 1

---
+++ src/main/java/io/e4i2/E4i2Application.java
@@ -0,0 +1,13 @@
+package io.e4i2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class E4i2Application {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(E4i2Application.class, args);
+    }
+    
+}
---
+++ src/test/java/io/e4i2/E4i2ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.e4i2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class E4i2ApplicationTests {
+    
+    @Test
+    void contextLoads() {
+    }
+    
+}
