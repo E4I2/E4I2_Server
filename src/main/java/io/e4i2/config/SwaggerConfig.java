3aecb5caa3e5b664fede6070bdcd8e299266de51
PeachCoolpis <rlglxh@naver.com>
2024-07-23 17:44:54 +0900

    fix. 스웨거 오류 수정

--- src/main/java/io/e4i2/config/SwaggerConfig.java
+++ src/main/java/io/e4i2/config/SwaggerConfig.java
@@ -1,14 +1,13 @@
 package io.e4i2.config;
 
-import org.springdoc.core.properties.AbstractSwaggerUiConfigProperties;
-import org.springdoc.core.properties.SwaggerUiConfigProperties;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.Collections;
 
 @Configuration
 public class SwaggerConfig {
@@ -17,17 +16,18 @@ public class SwaggerConfig {
     private String swagger;
     
     @Bean
-    @Primary
-    public SwaggerUiConfigProperties swaggerUiConfigProperties() {
-        SwaggerUiConfigProperties properties = new SwaggerUiConfigProperties();
-        AbstractSwaggerUiConfigProperties.SwaggerUrl swaggerUrl = new AbstractSwaggerUiConfigProperties.SwaggerUrl();
-        swaggerUrl.setName("Production Server");
-        swaggerUrl.setUrl(swagger);
-        
-        Set<AbstractSwaggerUiConfigProperties.SwaggerUrl> urls = new HashSet<>();
-        urls.add(swaggerUrl);
-        
-        properties.setUrls(urls);
-        return properties;
+    public OpenAPI customOpenAPI() {
+        Server server = new Server();
+        server.setUrl(swagger);
+        return new OpenAPI()
+                .servers(Collections.singletonList(server));
+    }
+    
+    @Bean
+    public GroupedOpenApi customOpenApi() {
+        return GroupedOpenApi.builder()
+                .group("e4i2-shop")
+                .pathsToMatch("/**")
+                .build();
     }
 }
