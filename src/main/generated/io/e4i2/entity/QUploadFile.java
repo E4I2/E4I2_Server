c8556bf5a3224752af218e74c9038763f40728e5
MUZI0960 <giy09060@gmail.com>
2024-07-23 22:43:11 +0900

    변경사항 커밋

---
+++ src/main/generated/io/e4i2/entity/QUploadFile.java
@@ -0,0 +1,45 @@
+package io.e4i2.entity;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+
+
+/**
+ * QUploadFile is a Querydsl query type for UploadFile
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QUploadFile extends EntityPathBase<UploadFile> {
+
+    private static final long serialVersionUID = -320102118L;
+
+    public static final QUploadFile uploadFile = new QUploadFile("uploadFile");
+
+    public final StringPath fileUrl = createString("fileUrl");
+
+    public final NumberPath<Long> id = createNumber("id", Long.class);
+
+    public final StringPath locationFilename = createString("locationFilename");
+
+    public final StringPath originalFilename = createString("originalFilename");
+
+    public final DateTimePath<java.time.LocalDateTime> uploadTime = createDateTime("uploadTime", java.time.LocalDateTime.class);
+
+    public QUploadFile(String variable) {
+        super(UploadFile.class, forVariable(variable));
+    }
+
+    public QUploadFile(Path<? extends UploadFile> path) {
+        super(path.getType(), path.getMetadata());
+    }
+
+    public QUploadFile(PathMetadata metadata) {
+        super(UploadFile.class, metadata);
+    }
+
+}
+
