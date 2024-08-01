fbc0e6f047adeca86864c73950cb24ea67c4090e
PeachCoolpis <rlglxh@naver.com>
2024-08-01 10:46:36 +0900

    fix. 검증오류 메시지 통일

--- src/main/java/io/e4i2/advice/ExceptionController.java
+++ src/main/java/io/e4i2/advice/ExceptionController.java
@@ -1,18 +1,15 @@
 package io.e4i2.advice;
 
 
+import io.e4i2.dto.ResponseDTO;
 import io.e4i2.response.ErrorResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.converter.HttpMessageNotReadableException;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-import java.util.List;
 
 @Slf4j
 @RestControllerAdvice
@@ -21,16 +18,19 @@ public class ExceptionController {
     
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ErrorResponse exceptionHandler(MethodArgumentNotValidException e) {
+    public ResponseDTO exceptionHandler(MethodArgumentNotValidException e) {
+//
+//        BindingResult bindingResult = e.getBindingResult();
+//        List<FieldError> fieldErrors = bindingResult.getFieldErrors();
+//
+//        ErrorResponse errorResponse = new ErrorResponse(HttpStatus.BAD_REQUEST.value(), "잘못된 요청입니다.");
+//
+//        fieldErrors.forEach(err -> errorResponse.addValidation(err.getField(), err.getDefaultMessage()));
+//
+//        return errorResponse;
+        log.error("MethodArgumentNotValidException = {}", e.getMessage());
+        return new ResponseDTO(new ResponseDTO.Result(400, "잘못된 요청입니다.", "fail"));
         
-        BindingResult bindingResult = e.getBindingResult();
-        List<FieldError> fieldErrors = bindingResult.getFieldErrors();
-        
-        ErrorResponse errorResponse = new ErrorResponse(HttpStatus.BAD_REQUEST.value(), "잘못된 요청입니다.");
-        
-        fieldErrors.forEach(err -> errorResponse.addValidation(err.getField(), err.getDefaultMessage()));
-        
-        return errorResponse;
     }
 //
 //    @ResponseStatus(HttpStatus.BAD_REQUEST)
@@ -42,17 +42,18 @@ public class ExceptionController {
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(Exception.class)
-    public ErrorResponse illegalArgumentExceptionHandler(Exception e) {
-        ErrorResponse errorResponse = new ErrorResponse(HttpStatus.BAD_REQUEST.value(), e.getMessage());
-        errorResponse.addValidation("serverError = ", errorResponse.getMessage());
-        return errorResponse;
+    public ResponseDTO illegalArgumentExceptionHandler(Exception e) {
+//        ErrorResponse errorResponse = new ErrorResponse(HttpStatus.BAD_REQUEST.value(), e.getMessage());
+//        errorResponse.addValidation("serverError = ", errorResponse.getMessage());
+//        return errorResponse;
+        log.error("Exception = {}", e.getMessage());
+        return new ResponseDTO(new ResponseDTO.Result(400, "잘못된 요청입니다.", "fail"));
     }
     
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(HttpMessageNotReadableException.class)
-    public ErrorResponse jsonExceptionHandler(HttpMessageNotReadableException e) {
-        ErrorResponse errorResponse = new ErrorResponse(HttpStatus.BAD_REQUEST.value(), "MBTI 값이 올바르지 않습니다.");
-        errorResponse.addValidation("mbti","MBTI 값이 올바르지 않습니다.");
-        return errorResponse;
+    public ResponseDTO jsonExceptionHandler(HttpMessageNotReadableException e) {
+        log.error("HttpMessageNotReadableException = {}", e.getMessage());
+        return new ResponseDTO(new ResponseDTO.Result(400, "잘못된 요청입니다.", "fail"));
     }
 }
