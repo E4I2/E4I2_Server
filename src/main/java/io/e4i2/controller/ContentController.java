86eb5475c6eea7d87b8e499348e4a599d81ab895
PeachCoolpis <rlglxh@naver.com>
2024-08-07 02:44:40 +0900

    fix. content Post로 수정

--- src/main/java/io/e4i2/controller/ContentController.java
+++ src/main/java/io/e4i2/controller/ContentController.java
@@ -6,10 +6,7 @@ import io.e4i2.request.ContentRequest;
 import io.e4i2.service.ContentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,7 +15,7 @@ public class ContentController {
     
     private final ContentService contentService;
     
-    @GetMapping
+    @PostMapping
     public ContentDTO content(@Validated @RequestBody ContentRequest contentRequest) {
         if (contentRequest.getContentId() >= 10) {
             return contentService.getContentPrompt(contentRequest);
